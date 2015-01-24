53d46aa3f7b18ebe0bdaf0061bc0218aa45afa4b
kremlin <ian@kremlin.cc>
2015-01-23 21:43:52 -0500

    start with httpd // fix timestamps on git

--- httpd.go
+++ httpd.go
@@ -1,9 +1,14 @@
-package main 
+package main
 
 import "fmt"
-/*import "net/http"*/
+import "net/http"
 
 func main() {
 
+    /* make sure to close() anything you need to (you need to) */
     fmt.Println("welp");
+    resp, err := http.Get("http://kremlin.cc")
+
+    if err != nil {}
+    fmt.Println(resp)
 }
